2708ba8c94bc951eb44e6f8c425fd0864ce76b3d
Bruno Bonomo <bruno.bonomo11@gmail.com>
2024-01-07 21:57:05 -0300

    Agregamos Descripcion detallada de cada producto para agregar al carrito y agregamos la opcion para cambiar el contador

--- src/Components/ItemDetail/ItemDetail.jsx
+++ src/Components/ItemDetail/ItemDetail.jsx
@@ -1,15 +1,96 @@
-import ItemCount from "../ItemCount/ItemCount"
+import { useState } from "react"
 
-const ItemDetail = ({name, category, img, stock, price, description}) => {
+const InputCount = ({onAdd, stock, initial= 1}) => {
+    const [count, setCount] = useState(initial)
+
+    const handleChange = (e) => {
+        if(e.target.value <= stock ) {
+            setCount(e.target.value)
+        }
+    }
+
     return (
         <div>
-            <h1>{name}</h1>
-            <p>categoria: {category}</p>
-            <img src={img} style={{ width: 200 }}/>
-            <h3>${price}</h3>
-            <p>Descripcion: {description}</p>
-            <ItemCount stock={stock} />
+            <input type="number" onChange={handleChange} value={count} />
+            <button onClick={() => onAdd(count)}>Agregar al carrito</button>
         </div>
+    )
+}
+
+const ButtonCount = ({onAdd, stock, initial = 1}) => {
+    const [count, setCount] = useState(initial)
+
+    const increment = () => {
+        if(count < stock) {
+            setCount(count + 1)
+        }
+    }
+
+    const decrement = () => {
+            setCount(count - 1)
+    }
+
+    return (
+        <div>
+            <p>{count}</p>
+            <button onClick={decrement}>-</button>
+            <button onClick={increment}>+</button>
+            <button onClick={() => onAdd(count)}>Agregar al carrito</button>
+        </div>
+    )
+}
+
+const ItemDetail = ({id, name, category, img, stock, price, description}) => {
+    const [inputType, setInputType] = useState('button')
+    const [quantity, setQuantity] = useState(0)
+
+    const ItemCount = inputType === 'input' ? InputCount : ButtonCount
+
+    const handleOnAdd = (count) => {
+        const objProducToadd = {
+            id, name, price, count
+        }
+        console.log(objProducToadd)
+        console.log('agregue al carrito: ', count)
+
+        setQuantity(count)
+    }
+
+
+    return (
+        <article>
+            <button onClick={() => setInputType(inputType === 'input' ? 'button' : 'input')}>
+                Cambiar contador
+            </button>
+            <header>
+                <h2>
+                    {name}
+                </h2>
+            </header>
+            <picture>
+                <img src={img} alt={name} style={{ width: 100}} />
+            </picture>
+            <section>
+                <p>
+                    Categoria: {category}
+                </p>
+                <p>
+                    Descripcion: {description}
+                </p>
+                <p>
+                    Precio: {price}
+                </p>
+            </section>
+            <footer>
+                {
+                    quantity === 0 ? (
+                        <ItemCount onAdd={handleOnAdd} stock={stock} />
+                    ) : (
+                        <button>Finalizar Compra</button>
+                    )
+                }
+            </footer>
+        </article>
     )
 }
 
